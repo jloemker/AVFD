55201e26da7c6c5dd1be5aca3f6a1165833ceb67
Johanna <jloemker@yahoo.de>
2021-09-22 14:12:17 +0200

    fixed the key problem

--- plots/PlotMacro.C
+++ plots/PlotMacro.C
@@ -8,20 +8,25 @@
 void PlotMacro(){
 //gStyle -> SetOptStat(0);
 //gROOT->SetBatch();
-TFile * File = new TFile("/user/jlomker/project/AVFD/result/AnalysisResult_a-0.1_5.44TeV.root");
+TFile* File = new TFile("/user/jlomker/project/AVFD/result/AnalysisResult_a-0.1_5.44TeV.root");
 
 //v2 = File.Get('FlowFromBWList/V2IntPro');//or V2IntProQC
 //gpp= File.Get('CMEList/')
-              
-TCanvas *c1 = new TCanvas("c1","pT",400,400);                  
-TDirectory *dir1 = (TDirectory*) File->Get("QAList;1");
-TH1F *pT = (TH1F*) dir1->Get("fPtChargedParticlesDistribution");
-//h_eta -> GetYaxis()->SetTitle("#eta AK8_{2}");
-//h_eta ->GetXaxis()->SetTitle("#eta AK8_{1}");
-//h_eta -> SetTitle("#eta AK8_{1} vs #eta AK8_{2}");
+//                 
+//Getting the list(s) of files
+TList *QA = (TList*) File->Get("QAList;1");
+//Extracting the histogram(s) from the list             
+TH1F *pT = (TH1F*) QA->FindObject("fPtChargedParticlesDistribution");
+//Define the Canvas for the plot
+TCanvas *c1 = new TCanvas("c1","pT",400,400);
+//
+// Some things to make things more professional 
+//pT -> GetYaxis()->SetTitle("#eta AK8_{2}");
+//pT ->GetXaxis()->SetTitle("#eta AK8_{1}");
+//pT -> SetTitle("#eta AK8_{1} vs #eta AK8_{2}");
 pT->Draw();
 c1->SaveAs("pT.pdf");
-
+/*
 TCanvas *c2 = new TCanvas("c2","Delta delta",400,400);
 TH1F *delDelta = (TH1F*) File->Get("CMEList;1/DeltaD11");
 delDelta->Draw();
@@ -31,4 +36,6 @@ TCanvas *c3 = new TCanvas("c3","Delta gamma", 400, 400);
 TH1F *delGamma = (TH1F*) File->Get("CMEList;1/DeltaG112");
 delGamma ->Draw();
 c3->SaveAs("delGamma.pdf");
+*/
 }
+
