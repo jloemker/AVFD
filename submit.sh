6e021f9b771d0d0c357e1b4d65da3a9c33ff903b
Johanna <jloemker@yahoo.de>
2021-09-25 20:59:40 +0200

    Seems to be running; confirmation after my run...

--- submit.sh
+++ submit.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 SCRIPT="runAnalysis.sh"
-runNumber=1
-while [ $runNumber -le 10 ]; do
-    echo "Adding run number from file: $runNumber"
+#runNumber=1
+#while [ $Cent_ID -le 8 ]; do
+echo "Opening the script"
 
 #make the script to submit
     (echo "#!/bin/bash"
 echo "source /cvmfs/alice.cern.ch/etc/login.sh"
-#VO_ALICE@ROOT::v6-24-06-3
-echo "eval $(alienv printenv VO_ALICE@AliPhysics::vAN-20210910-1)"
+
+#echo "source /cmvfs/alice.cern.ch/bin/alienv"
+
+#echo "eval $(alienv printenv VO_ALICE@AliPhysics::vAN-20210910-1)"
+echo "eval $(alienv printenv AliPhysics/vAN-20210915_ROOT6-1)"
 echo "which aliroot || exit 1"
-echo "mkdir -p /user/jlomker/project/AVFD/output/Set$runNumber"
-#echo "cd /user/jlomker/project/AVFD/output/Set$runNumber"
+#echo "mkdir -p /user/jlomker/project/AVFD/output"
+#echo "cd /user/jlomker/project/AVFD/output"
 #echo "mkdir -p ${TMPDIR}"
 echo "cd ${TMPDIR}"
 echo "pwd"
-echo "if [ -f AnalysisResults.root ]"
-echo "  then "
-echo "rm -rf AnalysisResults.root"
-echo "fi"
+#echo "if [ -f AnalysisResults_$Cent_ID ]"
+#echo "  then "
+#echo "rm -rf AnalysisResults_$Cent_ID"
+#echo "fi"
+#echo "if [! -f runSingleCentrality.C]"
+#echo "then "
+#echo "ln -s /user/jlomker/project/AVFD/runSingleCentrality.C ."
+#echo "fi"
 echo "if [ ! -f convert_tree_splitFiles.C ]"
 echo " then "
 echo "ln -s /user/jlomker/project/AVFD/convert_tree_splitFiles.C ." 
@@ -39,11 +46,14 @@ echo "if [ ! -f runSingleCentrality.C ]"
 echo " then "
 echo "ln -s /user/jlomker/project/AVFD/runSingleCentrality.C ."
 echo "fi"
-echo "exec aliroot -b -q runSingleCentrality.C'($runNumber)'"
+echo "exec aliroot -b -q convert_tree_splitFiles.C"
+echo "exec aliroot -b -q CalulateFlowCME.cxx"
+echo "exec aliroot -b -q runSingleCentrality.C"
     ) > $SCRIPT
+echo "success !"
 
 qsub -q generic $SCRIPT 
 
-let runNumber++
+#let runNumber++
 
-done
+#done
